35d5abef5520fc6eec4e68f75126772ceee7b5e4
EsaulAlfacan <esaulalfacan@gmail.com>
2024-03-09 16:17:18 -0600

    Ejercicios a resolver: Lógica de programación

--- JavaScript/003_logica_de_programacion.js
+++ JavaScript/003_logica_de_programacion.js
@@ -1,41 +1,5 @@
 // 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
-const NUMERO_ALEATORIO = () => parseInt(Math.random() * 100) + 500;
-console.log(NUMERO_ALEATORIO());
-console.log(NUMERO_ALEATORIO());
 
 // 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
-const NUMERO_CAPICUA = (numero) => {
-  let numeroConvertido = numero.toString().split("").reverse();
-  let numeroNormal = numero.toString().split("");
-
-  console.log(numeroConvertido, numeroNormal);
-
-  if (!numero) {
-    return "No es un número";
-  } else if (typeof numero !== "number") {
-    return "No es un número";
-  } else if (numeroConvertido.join("") === numeroNormal.join("")) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-console.log(NUMERO_CAPICUA(2002));
 
 // 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
-const FACTORIAL = function (num) {
-  if (!num) {
-    return "No ingresaste un número";
-  } else if (typeof num !== "number") {
-    return "No es un número";
-  } else {
-    let resultado = 1;
-    for (let i = 1; i <= num; i++) {
-      resultado *= i;
-    }
-    return resultado;
-  }
-};
-
-console.log(FACTORIAL(5));
--- JavaScript/004_logica_de_programacion.js
+++ JavaScript/004_logica_de_programacion.js
@@ -1,41 +1,10 @@
 // 12) Programa una función que determine si un número es primo (aquel que solo es divisible por sí mismo y 1) o no, pe. miFuncion(7) devolverá true.
 
-const FUN_NUM_PRIMO = (num) => {
-  if (!num) return "No ingresaste un númnero";
-  if (typeof num !== "number") return `El dato ${num} no es un número`;
-  if (num <= 0) return "El número no puede ser igual o menor que 0";
-  return num % num === 0 && num % 1 === 0 && num % 2 !== 0
-    ? "El número es primo"
-    : `El número ${num} no es primo`;
-};
-
-console.log(FUN_NUM_PRIMO(2));
 
 // 13) Programa una función que determine si un número es par o impar, pe. miFuncion(29) devolverá Impar.
-const NUM_PAR_IMPAR = (num1) => {
-  if (!num1) return "No ingresaste un númnero";
-  if (typeof num1 !== "number") return `El dato ${num1} no es un número`;
-  if (num1 <= 0) return "El número no puede ser igual o menor que 0";
 
-  return num1 % num1 === 0 && num1 % 2 === 0
-    ? "El número es par"
-    : `El número ${num1} no es par`;
-};
-
-console.log(NUM_PAR_IMPAR(53));
 
 // 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe.
 //miFuncion(0, "C") devolverá 32°F.
-
 //°C = (°F - 32) x 5/9
 
-const CONVERTIR_C_A = (num2) => {
-  if (typeof num2 !== "number") return `El dato ${num2} no es un número`;
-
-  let aCelcisu = (num2 - 32) * (5 / 9);
-  let aFaren = num2 * (5 / 9) + 32;
-
-  return `La temperatura de ${num2} unidades, corresponde a ${aCelcisu}°C y a ${aFaren}°F`;
-};
-
-console.log(CONVERTIR_C_A(0))
